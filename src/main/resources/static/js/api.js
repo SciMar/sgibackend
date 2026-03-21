3995f943c9dc56496b0cd8c61ee7ed09ef9d2130
SciMar <cramirezanzola@gmail.com>
2026-03-21 07:09:44 -0500

    [feat] conf api_url

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 // Cambia esta URL por la de tu backend
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://sgibackend-production.up.railway.app';
 
 // ==========================================
 // OBTENER HEADERS CON AUTENTICACIÓN
